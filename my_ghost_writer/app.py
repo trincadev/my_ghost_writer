9f7342c5ccfd33239f04fa34f5404a1d3c0ae464
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2025-05-08 19:01:56 +0200

    feat: add STATIC_FOLDER, API_MODE env variables

--- my_ghost_writer/app.py
+++ my_ghost_writer/app.py
@@ -7,7 +7,7 @@ from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-from my_ghost_writer.constants import ALLOWED_ORIGIN_LIST, DOMAIN, IS_TESTING, LOG_LEVEL, PORT, STATIC_FOLDER, app_logger
+from my_ghost_writer.constants import ALLOWED_ORIGIN_LIST, API_MODE, DOMAIN, IS_TESTING, LOG_LEVEL, PORT, STATIC_FOLDER, app_logger
 from my_ghost_writer.type_hints import RequestTextFrequencyBody
 
 
@@ -60,22 +60,35 @@ def get_words_frequency(body: RequestTextFrequencyBody | str) -> JSONResponse:
     return JSONResponse(status_code=200, content=content_response)
 
 
-app.mount("/static", StaticFiles(directory=STATIC_FOLDER, html=True), name="static")
+try:
+    app.mount("/static", StaticFiles(directory=STATIC_FOLDER, html=True), name="static")
+except Exception as ex_mount_static:
+    app_logger.error(f"Failed to mount static folder: {STATIC_FOLDER}, exception: {ex_mount_static}!")
+    if not API_MODE:
+        app_logger.exception(f"since API_MODE is {API_MODE} we will raise the exception!")
+        raise ex_mount_static
 
 # add the CorrelationIdMiddleware AFTER the @app.middleware("http") decorated function to avoid missing request id
 app.add_middleware(CorrelationIdMiddleware)
 
 
-@app.get("/")
-@app.get("/static/")
-def index() -> FileResponse:
-    return FileResponse(path=STATIC_FOLDER / "index.html", media_type="text/html")
+try:
+    @app.get("/")
+    @app.get("/static/")
+    def index() -> FileResponse:
+        return FileResponse(path=STATIC_FOLDER / "index.html", media_type="text/html")
+except Exception as ex_route_main:
+    app_logger.error(f"Failed to prepare the main route, exception: {ex_route_main}!")
+    if not API_MODE:
+        app_logger.exception(f"since API_MODE is {API_MODE} we will raise the exception!")
+        raise ex_route_main
 
 
 if __name__ == "__main__":
     try:
-        uvicorn.run("app:app", host=DOMAIN, port=PORT, reload=bool(IS_TESTING))
-    except Exception as ex:
-        print(f"fastapi/gradio application {fastapi_title}, exception:{ex}!")
-        app_logger.exception(f"fastapi/gradio application {fastapi_title}, exception:{ex}!")
-        raise ex
+        app_logger.info(f"Starting fastapi/gradio application {fastapi_title}, run in api mode: {API_MODE}...")
+        uvicorn.run("my_ghost_writer.app:app", host=DOMAIN, port=PORT, reload=bool(IS_TESTING))
+    except Exception as ex_run:
+        print(f"fastapi/gradio application {fastapi_title}, exception:{ex_run}!")
+        app_logger.exception(f"fastapi/gradio application {fastapi_title}, exception:{ex_run}!")
+        raise ex_run
--- my_ghost_writer/constants.py
+++ my_ghost_writer/constants.py
@@ -9,11 +9,13 @@ from my_ghost_writer import session_logger
 load_dotenv()
 PROJECT_ROOT_FOLDER = Path(__file__).parent.parent
 STATIC_FOLDER = PROJECT_ROOT_FOLDER / "static"
+STATIC_FOLDER = Path(os.getenv("STATIC_FOLDER", str(STATIC_FOLDER)))
 DOMAIN=os.getenv("DOMAIN", "localhost")
 PORT=int(os.getenv("PORT", 7860))
 ALLOWED_ORIGIN_LIST = os.getenv('ALLOWED_ORIGIN', f'http://{DOMAIN}:{PORT}').split(",")
 LOG_JSON_FORMAT = bool(os.getenv("LOG_JSON_FORMAT"))
 IS_TESTING = bool(os.getenv('IS_TESTING', ""))
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+API_MODE = bool(os.getenv("API_MODE", ""))
 session_logger.setup_logging(json_logs=LOG_JSON_FORMAT, log_level=LOG_LEVEL)
 app_logger = structlog.stdlib.get_logger(__name__)
