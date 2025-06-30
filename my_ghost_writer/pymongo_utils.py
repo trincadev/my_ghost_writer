c45d62b360ec384836c9ec77daea120211da88c5
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2025-06-30 16:55:39 +0200

    feat: update get_client() from pymongo_utils.py

--- my_ghost_writer/pymongo_utils.py
+++ my_ghost_writer/pymongo_utils.py
@@ -1,10 +1,19 @@
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 from my_ghost_writer.constants import DEFAULT_DBNAME_THESAURUS, ME_CONFIG_MONGODB_URL, DEFAULT_COLLECTION_THESAURUS, ME_CONFIG_MONGODB_TIMEOUT, app_logger
 
 
 def get_client() -> MongoClient:
-    client = MongoClient(ME_CONFIG_MONGODB_URL, timeoutMS=ME_CONFIG_MONGODB_TIMEOUT)
+    client = MongoClient(ME_CONFIG_MONGODB_URL, timeoutMS=ME_CONFIG_MONGODB_TIMEOUT, server_api=ServerApi('1'))
+
+    try:
+        client.admin.command('ping', check=True)
+        app_logger.info("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        app_logger.error(f"failed to connect to mongodb: {e}!")
+        raise e
+
     return client
 
 
@@ -20,8 +29,6 @@ def get_thesaurus_collection(db_name: str = DEFAULT_DBNAME_THESAURUS, collection
 
 def mongodb_health_check(db_name: str = DEFAULT_DBNAME_THESAURUS, collection_name: str = DEFAULT_COLLECTION_THESAURUS) -> bool:
     client = get_client()
-    # Check server is available
-    client.admin.command('ping', check=True)
     server_info = client.server_info()
     server_version = server_info["version"]
     app_logger.info(f"mongodb server_version:{server_version}!")
